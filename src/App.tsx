bad4509876b328332a82351028b18a466e3134fa
hamdi2050 <hamdi2050@live.com>
2023-06-15 08:28:50 +0100

    Import bootstrap JS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+// Import all of Bootstrap's JS
+import "bootstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
