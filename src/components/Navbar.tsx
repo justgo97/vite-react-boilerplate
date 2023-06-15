3a76f3620b9d0f6ad2948b56bd09b0c196651597
hamdi2050 <hamdi2050@live.com>
2023-06-15 16:26:31 +0100

    Update default component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid justify-content-center">
+        <span className="navbar-text fs-4 fw-bold text-success">
+          Vite + React boilerplate
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
